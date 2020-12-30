a413b7a1dc6a8551963da47b2c715a7f708b14a0
Paden Allen <padenallen15@yahoo.com>
2020-12-30 17:43:37 -0600

    added one question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,64 @@
 var timerEl = document.getElementById('timer');
+var timeLeft = 20;
+var tempTime = 0;
+var score = 0;
 var startBtn = document.getElementById('start-quiz');
+var openState = document.getElementById('opening-statement');
+var quizQuest = document.getElementById('quiz-question');
+var questTitle = document.getElementById('question');
+var answerA = document.getElementById('answerA');
+var answerB = document.getElementById('answerB');
+var answerC = document.getElementById('answerC');
+var answerD = document.getElementById('answerD');
+var resultSection = document.getElementById('the-result');
+var result = document.getElementById('result');
+
+function resetAnswers() {
+    answerA.style.color = 'black';
+    answerB.style.color = 'black';
+    answerC.style.color = 'black';
+    answerD.style.color = 'black';
+}
+
+function firstQuestion() {
+    questTitle.textContent = 'This is the first question';
+    answerA.textContent = 'Wrong';
+    answerB.textContent = 'Wrong';
+    answerC.textContent = 'Right';
+    answerD.textContent = 'Wrong';
+
+    answerA.addEventListener("click", function() {
+        tempTime = timeLeft - 5;
+        answerA.style.color = 'red';
+        answerC.style.color = 'green';
+        resultSection.style.display = 'block';
+        resultSection.style.borderColor = 'red';
+        result.textContent = 'Wrong Answer, Time -5 Seconds!';
+        setTimeout(secondQuestion, 3000);
+    });
+
+}
+
+function secondQuestion() {
+    resetAnswers();
+    timeLeft = tempTime;
+    resultSection.style.display = 'none';
+    questTitle.textContent = 'This is the second question';
+    answerA.textContent = 'Wrong';
+    answerB.textContent = 'Wrong';
+    answerC.textContent = 'Wrong';
+    answerD.textContent = 'Right';
+}
+
+
+
 
 // Timer that counts down from 60
 function countdown() {
-    var timeLeft = 5;
+    startBtn.style.display = 'none';
+    openState.style.display = 'none';
+    quizQuest.style.display = 'block';
+    firstQuestion();
   
     // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
     var timeInterval = setInterval(function() {
